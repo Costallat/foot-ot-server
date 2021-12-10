40d4c45e272bbef933eacd3cdac7494394f23a96
Renato Foot <costallat@hotmail.com>
2021-12-10 17:29:56 -0300

    Add wait for DB and test accounts
    - Waits for DB to answer before trying to create database
    - Add step to create test account when needed
    
    Signed-off-by: Renato Foot <costallat@hotmail.com>

--- docker/server/start.sh
+++ docker/server/start.sh
@@ -9,6 +9,7 @@ OT_SERVER_IP="${SERVER_IP:-127.0.0.1}"
 OT_SERVER_LOGIN_PORT="${SERVER_LOGIN_PORT:-7171}"
 OT_SERVER_GAME_PORT="${SERVER_GAME_PORT:-7172}"
 OT_SERVER_STATUS_PORT="${SERVER_STATUS_PORT:-7171}"
+OT_SERVER_TEST_ACCOUNTS="${SERVER_TEST_ACCOUNTS:-false}"
 
 echo ""
 echo "===== Print Variables ====="
@@ -29,6 +30,20 @@ echo "================================"
 echo ""
 
 echo ""
+echo "===== Wait for the DB to be Up ====="
+echo ""
+
+until mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST --port=$DB_PORT -e "SHOW DATABASES;"
+do
+	echo "DB offline, trying again"
+	sleep 5s
+done
+
+echo ""
+echo "================================"
+echo ""
+
+echo ""
 echo "===== Create Database and Import schema ====="
 echo ""
 
@@ -40,6 +55,28 @@ echo ""
 echo "================================"
 echo ""
 
+
+echo ""
+echo "===== Check and Create Test Accounts ====="
+echo ""
+
+if [ "$OT_SERVER_TEST_ACCOUNTS" = "true" ]
+then
+	echo "Creating Test Accounts..."
+	mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST --port=$DB_PORT -D $DB_DATABASE < 01-test_account.sql
+	mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST --port=$DB_PORT -D $DB_DATABASE < 02-test_account_players.sql
+else
+	echo "Skip Test Account creation!"
+fi
+
+echo ""
+echo "================================"
+echo ""
+
+echo ""
+echo "===== Server Configuration ====="
+echo ""
+
 sed -i '/mysqlHost = .*$/c\mysqlHost = "'$DB_HOST'"' config.lua
 sed -i '/mysqlUser = .*$/c\mysqlUser = "'$DB_USER'"' config.lua
 sed -i '/mysqlPass = .*$/c\mysqlPass = "'$DB_PASSWORD'"' config.lua
@@ -49,10 +86,6 @@ sed -i '/ip = .*$/c\ip = "'$OT_SERVER_IP'"' config.lua
 sed -i '/loginProtocolPort = .*$/c\loginProtocolPort = '$OT_SERVER_LOGIN_PORT'' config.lua
 sed -i '/gameProtocolPort = .*$/c\gameProtocolPort = '$OT_SERVER_GAME_PORT'' config.lua
 sed -i '/statusProtocolPort = .*$/c\statusProtocolPort = '$OT_SERVER_STATUS_PORT'' config.lua
-
-echo ""
-echo "===== Server Configuration ====="
-echo ""
 
 cat config.lua
 
@@ -66,4 +99,3 @@ echo ""
 
 ulimit -c unlimited
 canary
-
