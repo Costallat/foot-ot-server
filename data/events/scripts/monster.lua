42684ba6b1a163fb8428088a224776059e5ade91
Renato Foot <costallat@hotmail.com>
2021-12-13 10:11:02 -0300

    Improve start.sh double quote variables
    Double quote variables to avoid globbing and word splitting.
    
    Signed-off-by: Renato Foot <costallat@hotmail.com>

--- data/events/scripts/monster.lua
+++ data/events/scripts/monster.lua
@@ -1,51 +1,58 @@
 function Monster:onDropLoot(corpse)
-	if configManager.getNumber(configKeys.RATE_LOOT) == 0 then
-		return
-	end
+    if configManager.getNumber(configKeys.RATE_LOOT) == 0 then return end
 
-	local player = Player(corpse:getCorpseOwner())
-	local mType = self:getType()
-	if not player or player:getStamina() > 840 then
-		local monsterLoot = mType:getLoot()
-		for i = 1, #monsterLoot do
-			local item = corpse:createLootItem(monsterLoot[i])
-			if not item then
-				Spdlog.info("Could not add loot item to corpse of monster '".. mType:getName() .."'")
-			end
-		end
+    local player = Player(corpse:getCorpseOwner())
+    local mType = self:getType()
+    if not player or player:getStamina() > 840 then
+        local monsterLoot = mType:getLoot()
+        for i = 1, #monsterLoot do
+            local item = corpse:createLootItem(monsterLoot[i])
+            if not item then
+                Spdlog.info("Could not add loot item to corpse of monster '" ..
+                                mType:getName() .. "'")
+            end
+        end
 
-		if player then
-			local text = {}
-			local oldClientText = ""
-			local version = player:getClient().version
-			if self:getName():lower() == (Game.getBoostedCreature()):lower() then
-				 text = ("Loot of %s: %s (boosted loot)"):format(mType:getNameDescription(), corpse:getContentDescription())
-				 if party or version < 1200 then
-					oldClientText = ("Loot of %s: %s (boosted loot)"):format(mType:getNameDescription(), corpse:getContentDescription(true))
-				 end
-			else
-				 text = ("Loot of %s: %s"):format(mType:getNameDescription(), corpse:getContentDescription())
-				 if party or version < 1200 then
-					oldClientText = ("Loot of %s: %s"):format(mType:getNameDescription(), corpse:getContentDescription(true))
-				 end
-			end
-			local party = player:getParty()
-			if party then
-				party:broadcastPartyLoot(text, oldClientText)
-			else
-				player:sendTextMessage(MESSAGE_LOOT, text)
-				if version < 1200 then
-					player:sendTextMessage(MESSAGE_GUILD, oldClientText, 9)
-				end
-			end
-		end
-	else
-		local text = ("Loot of %s: nothing (due to low stamina)"):format(mType:getNameDescription())
-		local party = player:getParty()
-		if party then
-			party:broadcastPartyLoot(text)
-		else
-			player:sendTextMessage(MESSAGE_LOOT, text)
-		end
-	end
+        if player then
+            local text = {}
+            local oldClientText = ""
+            local version = player:getClient().version
+            if self:getName():lower() == (Game.getBoostedCreature()):lower() then
+                text = ("Loot of %s: %s (boosted loot)"):format(
+                           mType:getNameDescription(),
+                           corpse:getContentDescription())
+                if party or version < 1200 then
+                    oldClientText = ("Loot of %s: %s (boosted loot)"):format(
+                                        mType:getNameDescription(),
+                                        corpse:getContentDescription(true))
+                end
+            else
+                text = ("Loot of %s: %s"):format(mType:getNameDescription(),
+                                                 corpse:getContentDescription())
+                if party or version < 1200 then
+                    oldClientText = ("Loot of %s: %s"):format(
+                                        mType:getNameDescription(),
+                                        corpse:getContentDescription(true))
+                end
+            end
+            local party = player:getParty()
+            if party then
+                party:broadcastPartyLoot(text, oldClientText)
+            else
+                player:sendTextMessage(MESSAGE_LOOT, text)
+                if version < 1200 then
+                    player:sendTextMessage(MESSAGE_GUILD, oldClientText, 9)
+                end
+            end
+        end
+    else
+        local text = ("Loot of %s: nothing (due to low stamina)"):format(
+                         mType:getNameDescription())
+        local party = player:getParty()
+        if party then
+            party:broadcastPartyLoot(text)
+        else
+            player:sendTextMessage(MESSAGE_LOOT, text)
+        end
+    end
 end
--- docker/server/start.sh
+++ docker/server/start.sh
@@ -33,7 +33,7 @@ echo ""
 echo "===== Wait for the DB to be Up ====="
 echo ""
 
-until mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST --port=$DB_PORT -e "SHOW DATABASES;"
+until mysql -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" --port="$DB_PORT" -e "SHOW DATABASES;"
 do
 	echo "DB offline, trying again"
 	sleep 5s
@@ -47,9 +47,9 @@ echo ""
 echo "===== Create Database and Import schema ====="
 echo ""
 
-mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST --port=$DB_PORT -e "CREATE DATABASE $DB_DATABASE;"
-mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST --port=$DB_PORT -e "SHOW DATABASES;"
-mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST --port=$DB_PORT -D $DB_DATABASE < schema.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" --port="$DB_PORT" -e "CREATE DATABASE $DB_DATABASE;"
+mysql -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" --port="$DB_PORT" -e "SHOW DATABASES;"
+mysql -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" --port="$DB_PORT" -D "$DB_DATABASE" < schema.sql
 
 echo ""
 echo "================================"
@@ -63,8 +63,8 @@ echo ""
 if [ "$OT_SERVER_TEST_ACCOUNTS" = "true" ]
 then
 	echo "Creating Test Accounts..."
-	mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST --port=$DB_PORT -D $DB_DATABASE < 01-test_account.sql
-	mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST --port=$DB_PORT -D $DB_DATABASE < 02-test_account_players.sql
+	mysql -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" --port="$DB_PORT" -D "$DB_DATABASE" < 01-test_account.sql
+	mysql -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" --port="$DB_PORT" -D "$DB_DATABASE" < 02-test_account_players.sql
 else
 	echo "Skip Test Account creation!"
 fi
@@ -77,15 +77,15 @@ echo ""
 echo "===== Server Configuration ====="
 echo ""
 
-sed -i '/mysqlHost = .*$/c\mysqlHost = "'$DB_HOST'"' config.lua
-sed -i '/mysqlUser = .*$/c\mysqlUser = "'$DB_USER'"' config.lua
-sed -i '/mysqlPass = .*$/c\mysqlPass = "'$DB_PASSWORD'"' config.lua
-sed -i '/mysqlPort = .*$/c\mysqlPort = '$DB_PORT'' config.lua
-sed -i '/mysqlDatabase = .*$/c\mysqlDatabase = "'$DB_DATABASE'"' config.lua
-sed -i '/ip = .*$/c\ip = "'$OT_SERVER_IP'"' config.lua
-sed -i '/loginProtocolPort = .*$/c\loginProtocolPort = '$OT_SERVER_LOGIN_PORT'' config.lua
-sed -i '/gameProtocolPort = .*$/c\gameProtocolPort = '$OT_SERVER_GAME_PORT'' config.lua
-sed -i '/statusProtocolPort = .*$/c\statusProtocolPort = '$OT_SERVER_STATUS_PORT'' config.lua
+sed -i "/mysqlHost = .*$/c\mysqlHost = \"$DB_HOST\"" config.lua
+sed -i "/mysqlUser = .*$/c\mysqlUser = \"$DB_USER\"" config.lua
+sed -i "/mysqlPass = .*$/c\mysqlPass = \"$DB_PASSWORD\"" config.lua
+sed -i "/mysqlPort = .*$/c\mysqlPort = $DB_PORT" config.lua
+sed -i "/mysqlDatabase = .*$/c\mysqlDatabase = \"$DB_DATABASE\"" config.lua
+sed -i "/ip = .*$/c\ip = \"$OT_SERVER_IP\"" config.lua
+sed -i "/loginProtocolPort = .*$/c\loginProtocolPort = \"$OT_SERVER_LOGIN_PORT\"" config.lua
+sed -i "/gameProtocolPort = .*$/c\gameProtocolPort = $OT_SERVER_GAME_PORT" config.lua
+sed -i "/statusProtocolPort = .*$/c\statusProtocolPort = $OT_SERVER_STATUS_PORT" config.lua
 
 cat config.lua
 
